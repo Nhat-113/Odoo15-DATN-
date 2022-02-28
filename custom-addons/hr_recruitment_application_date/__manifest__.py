ed1a24f7e80b22b59fefed2d4bc07d939b972031
unknown <vietlx@d-soft.com.vn>
2022-02-28 14:11:47 +0700

    Add field Application Date to Recruitment

---
+++ custom-addons/hr_recruitment_application_date/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'HR Recruitment Add Field',
+    'summary': 'Add field Application Date To Screen Create Job Position',
+    'version': "1.1.0",
+    'category': 'Human Resources/Recruitment',
+    'author': 'vietlx',
+    'website': 'https://d-soft.com.vn/',
+    'license': 'AGPL-3',
+    'depends': [
+        'hr',
+        'hr_recruitment'
+    ],
+    'data': [
+        'views/hr_application_date_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False
+}
