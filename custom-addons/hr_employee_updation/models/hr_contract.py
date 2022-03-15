5402f01694684469cae2111cff74bb152b4949ff
namphuonght <trannamphuong2k@gmail.com>
2022-03-15 17:29:19 +0700

    fix Odoo Server Error xảy ra khi tạo/sửa hợp đồng là không có ngày kết thúc

--- custom-addons/hr_employee_updation/models/hr_contract.py
+++ custom-addons/hr_employee_updation/models/hr_contract.py
@@ -9,11 +9,12 @@ class Contract(models.Model):
     @api.constrains('date_start', 'date_end')
     def _check_dates(self):
         for contract in self:
-            if contract.date_end and contract.date_start > contract.date_end or contract.date_end < date.today():
-                raise ValidationError(_(
-                    'Contract %(contract)s: start date (%(start)s) must be earlier than contract end date (%(end)s) and end date (%(end)s) must be later than today (%(today)s)',
-                    contract=contract.name, start=contract.date_start, end=contract.date_end, today=date.today()
-                ))
+            if contract.date_end:
+                if contract.date_end and contract.date_start > contract.date_end or contract.date_end < date.today():
+                    raise ValidationError(_(
+                        'Contract %(contract)s: start date (%(start)s) must be earlier than contract end date (%(end)s) and end date (%(end)s) must be later than today (%(today)s)',
+                        contract=contract.name, start=contract.date_start, end=contract.date_end, today=date.today()
+                    ))
 
     @api.constrains('employee_id')
     def _check_employee(self):
@@ -24,10 +25,11 @@ class Contract(models.Model):
 
     def _assign_open_contract(self):
         for contract in self:
-            if contract.date_end >= date.today():
-                contract.employee_id.sudo().write({'contract_id': contract.id})
-            else:
-                raise ValidationError(_('Contract has expired on day(%(date_end)s)', date_end=contract.date_end))
+            if contract.date_end:
+                if contract.date_end >= date.today():
+                    contract.employee_id.sudo().write({'contract_id': contract.id})
+                else:
+                    raise ValidationError(_('Contract has expired on day(%(date_end)s)', date_end=contract.date_end))
 
     def write(self, vals):
         res = super(Contract, self).write(vals)
