d87b3c602acc7cb1de6e6c88b6503fe284a2492f
namphuonght <trannamphuong2k@gmail.com>
2022-03-24 16:59:43 +0700

    Fix Không tính được lương khi hợp đồng có thời gian bắt đầu từ năm ngoái

--- custom-addons/hr_payroll_community/models/hr_payslip.py
+++ custom-addons/hr_payroll_community/models/hr_payslip.py
@@ -93,10 +93,9 @@ class HrPayslip(models.Model):
             raise ValidationError(_("Payslip 'Date From' must be earlier 'Date To'."))
 
         if self.contract_id.date_end:
-            if self.contract_id.date_start > self.date_from or self.contract_id.date_end < self.date_to:
-                if self.contract_id.date_start.month > self.date_from.month or self.contract_id.date_end.month < self.date_to.month :
-                    raise ValidationError(_('The following employees have a contract outside of the payslip period : %(name)s',
-                    name=self.employee_id.name))
+            if self.contract_id.date_start >= self.date_to or self.contract_id.date_end <= self.date_from:
+                raise ValidationError(_('The following employees have a contract outside of the payslip period : %(name)s',
+                name=self.employee_id.name))
 
     @api.constrains('name')
     def _check_payslips(self):
