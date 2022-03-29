7deb38b6a0232cfaacada584b3cf5885bac0bf85
namphuonght <trannamphuong2k@gmail.com>
2022-03-29 15:45:55 +0700

    Update export all field in Payroll

--- custom-addons/ds_export_excel/models/report_payslip_xls.py
+++ custom-addons/ds_export_excel/models/report_payslip_xls.py
@@ -19,10 +19,30 @@ class PayslipXlsx(models.AbstractModel):
             "Tên Nhân Viên",
             "Số tài khoản ngân hàng",
             "Tên CN ngân hàng",
-            "Tiền công đoàn",
-            "BHYT, BHXH, BHTN",
-            "Thuế TNCN",
+            "Phụ cấp không chịu thuế",
+            "Phụ cấp chịu thuế",
+            "Lương NET",
+            "Ngày công trong tháng",
+            "Bình quân ngày công",
+            "Ngày nghỉ không lương",
+            "Ngày công tính lương",
+            "Lương trong tháng",
+            "Thời Gian OT 100% Lương",
+            "Thời Gian OT 150% Lương",
+            "Thời Gian OT 200% Lương",
+            "Thời Gian OT 300% Lương",
             "Lương OT",
+            "Lương tạm ứng",
+            "Khoản bổ sung",
+            "Tiền công đoàn",
+            "Lương OT chịu thuế",
+            "Tổng thu nhâp trong tháng",
+            "BHYT, BHXH, BHTN",
+            "Giảm trừ gia cảnh và tiêu dùng cá nhân",
+            "Lương tháng 13/Bonus",
+            "Thu nhập chịu thế theo lương",
+            "Thuế TNCN theo lương",
+            "Thuế TNCN theo lương tháng 13/bonus",
             "Lương thực lĩnh",
         ]
 
@@ -41,20 +61,60 @@ class PayslipXlsx(models.AbstractModel):
             ot = list(filter(lambda line: line['code'] == 'OT', obj.line_ids))
             net = list(
                 filter(lambda line: line['code'] == 'NET', obj.line_ids))
+            pck = list(filter(lambda line: line['code'] == 'PCK', obj.line_ids))
+            pc = list(filter(lambda line: line['code'] == 'PC', obj.line_ids))
+            basic = list(filter(lambda line: line['code'] == 'Basic', obj.line_ids))
+            nctt = list(filter(lambda line: line['code'] == 'NCTT', obj.line_ids))
+            bqnc = list(filter(lambda line: line['code'] == 'BQNC', obj.line_ids))
+            nnkl = list(filter(lambda line: line['code'] == 'NNKL', obj.line_ids))
+            nctte = list(filter(lambda line: line['code'] == 'NCTTE', obj.line_ids))
+            tl = list(filter(lambda line: line['code'] == 'TL', obj.line_ids))
+            tot1 = list(filter(lambda line: line['code'] == 'TOT1', obj.line_ids))
+            tot2 = list(filter(lambda line: line['code'] == 'TOT2', obj.line_ids))
+            tot3 = list(filter(lambda line: line['code'] == 'TOT3', obj.line_ids))
+            tot4 = list(filter(lambda line: line['code'] == 'TOT4', obj.line_ids))
+            ltu = list(filter(lambda line: line['code'] == 'LTU', obj.line_ids))
+            kbs = list(filter(lambda line: line['code'] == 'KBS', obj.line_ids))
+            otct = list(filter(lambda line: line['code'] == 'OTCT', obj.line_ids))
+            ttntt = list(filter(lambda line: line['code'] == 'TTNTT', obj.line_ids))
+            gtgc = list(filter(lambda line: line['code'] == 'GTGC', obj.line_ids))
+            lbn = list(filter(lambda line: line['code'] == 'LBN', obj.line_ids))
+            tnc = list(filter(lambda line: line['code'] == 'TNC', obj.line_ids))
+            ttncnbn = list(filter(lambda line: line['code'] == 'TTNCNBN', obj.line_ids))
 
             col_values = [
                 obj.employee_id.name,
                 obj.employee_id.bank_account_id.acc_number or '',
                 obj.employee_id.bank_account_id.bank_name or '',
-                0 if len(pcd) == 0 else pcd[0].amount,
-                0 if len(bh) == 0 else bh[0].amount,
-                0 if len(ttncn) == 0 else ttncn[0].amount,
+                0 if len(pck) == 0 else pck[0].amount,
+                0 if len(pc) == 0 else pc[0].amount,
+                0 if len(basic) == 0 else basic[0].amount,
+                0 if len(nctt) == 0 else nctt[0].amount,
+                0 if len(bqnc) == 0 else bqnc[0].amount,
+                0 if len(nnkl) == 0 else nnkl[0].amount,
+                0 if len(nctte) == 0 else nctte[0].amount,
+                0 if len(tl) == 0 else tl[0].amount,
+                0 if len(tot1) == 0 else tot1[0].amount,
+                0 if len(tot2) == 0 else tot2[0].amount,
+                0 if len(tot3) == 0 else tot3[0].amount,
+                0 if len(tot4) == 0 else tot4[0].amount,
                 0 if len(ot) == 0 else ot[0].amount,
+                0 if len(ltu) == 0 else ltu[0].amount,
+                0 if len(kbs) == 0 else kbs[0].amount,
+                0 if len(pcd) == 0 else pcd[0].amount,
+                0 if len(otct) == 0 else otct[0].amount,
+                0 if len(ttntt) == 0 else ttntt[0].amount,
+                0 if len(bh) == 0 else bh[0].amount,
+                0 if len(gtgc) == 0 else gtgc[0].amount,
+                0 if len(lbn) == 0 else lbn[0].amount,
+                0 if len(tnc) == 0 else tnc[0].amount,
+                0 if len(ttncn) == 0 else ttncn[0].amount,
+                0 if len(ttncnbn) == 0 else ttncnbn[0].amount,
                 0 if len(net) == 0 else net[0].amount,
             ]
 
             for i, value in enumerate(col_values):
-                if i in [3, 4, 5, 6, 7]:
+                if i in range(3, 26):
                     sheet.write(row, col + i, value, currency_format)
                 else:
                     sheet.write(row, col + i, value, border)
