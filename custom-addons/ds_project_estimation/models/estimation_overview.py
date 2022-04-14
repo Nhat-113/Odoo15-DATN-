03bdf645f2fb51d562d5f9a39cf22316d04974ba
unknown <vietlx@d-soft.com.vn>
2022-04-14 13:58:20 +0700

    [Estimation][272] Fix Revision Field

--- custom-addons/ds_project_estimation/models/estimation_overview.py
+++ custom-addons/ds_project_estimation/models/estimation_overview.py
@@ -9,12 +9,17 @@ class EstimationOverview(models.Model):
     
     # Notebook and pages
     author = fields.Many2one('res.users', string="User Update", default=lambda self: self.env.user, readonly=True)
-    revision = fields.Char("Revision", readonly=True, copy=False, index=False, default="/")
+    revision = fields.Float("Revision", readonly=True, copy=False, index=False, default=0, digits= (1,1))
     description = fields.Text("Description", default="Nothing")
 
     @api.model
     def create(self, vals):
-        if vals.get("revision", "/") == "/":
-            vals["revision"] = self.env["ir.sequence"].next_by_code("estimation.overview") or "/"
-        result = super(EstimationOverview, self).create(vals)
-        return result
+        listRevision = self.env['estimation.overview'].search([])
+        revisionValue = 0
+        for item in listRevision:
+            if item.connect_overview.id == vals["connect_overview"]:
+                if revisionValue < item.revision:
+                    revisionValue = item.revision
+        if vals.get("revision", 0) == 0:
+            vals["revision"] = (revisionValue + 0.1)
+        return super(EstimationOverview, self).create(vals)
