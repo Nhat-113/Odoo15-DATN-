d2ff1ffefd38378b50e6b13ad6f4862c692764a3
dientd <dientd@d-soft.com.vn>
2022-05-05 12:32:06 +0700

    ADD: task score addons

---
+++ custom-addons/hr_employee_task_score/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "HR Employee Task Score",
+    "version": "15.0.0.1.0",
+    "category": "Human Resources",
+    "summary": "Employee",
+    "description": """
+
+    """,
+    "author": "dientd",
+    'website': 'https://d-soft.com.vn/',
+    "depends": ['hr', 'base', 'project', 'project_updation'],
+    "license": "AGPL-3",
+    "data": [
+        "views/employee_views.xml",
+        # "security/ir.model.access.csv",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
