9a4f356e280c7a8ebb2907f94f8bf1a5f52fe867
dientd <dientd@d-soft.com.vn>
2022-05-13 09:40:54 +0700

    Add task score on employee_view_tree

--- custom-addons/hr_employee_task_score/models/hr_employee.py
+++ custom-addons/hr_employee_task_score/models/hr_employee.py
@@ -6,7 +6,7 @@ from odoo import fields, models, api
 class HrEmployee(models.Model):
     _inherit = "hr.employee"
 
-    task_score = fields.Char('project.task', compute="_compute_avg_task_score")
+    task_score = fields.Char(string='Task Score', compute="_compute_avg_task_score")
 
     @api.onchange('task_score')
     def _compute_avg_task_score(self):
@@ -15,10 +15,12 @@ class HrEmployee(models.Model):
         for employee in self:
             list_score = []
             for task in tasks:
-                if employee.id in task.user_ids.ids:
+                if employee.user_id.id in task.user_ids.ids:
                     list_score.append(int(task.task_score))
             if len(list_score):
-                employee.task_score = str(round(sum(list_score)/len(list_score), 1))
+                employee.task_score = round(sum(list_score)/len(list_score), 1)
+                if employee.task_score == '0.0':
+                    employee.task_score = '0'
             else:
-                employee.task_score = 0
+                employee.task_score = '0'
 
