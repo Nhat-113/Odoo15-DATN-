09a8c554bcea10941e1e722570e35d426ad54c57
namphuonght <trannamphuong2k@gmail.com>
2022-05-31 11:51:47 +0700

    fix duration

--- custom-addons/ds_project_planning/models/project_task.py
+++ custom-addons/ds_project_planning/models/project_task.py
@@ -22,7 +22,7 @@ class ProjectTask(models.Model):
     milestone_id = fields.Many2one(
         'project.planning.milestone', string='Milestone', required=False, domain=[('phase_id', '=', phase_id)], help="Project Milestone")
 
-    planned_duration = fields.Float('Duration', default=0, compute='_compute_planned_duration', store=True, readonly=True)
+    planned_duration = fields.Float('Duration', default=0, compute='_compute_planned_duration', inverse='_inverse_planned_duration', store=True, readonly=True)
     working_day = fields.Float('Working Day', default=0, compute='_compute_working_day', store=True, readonly=True)
     lag_time = fields.Integer('Lag Time')
     depending_task_ids = fields.One2many('project.depending.tasks', 'task_id')
@@ -78,14 +78,11 @@ class ProjectTask(models.Model):
                 r.working_day = round(elapsed_seconds / seconds_in_day, 1)
 
 
-    # @api.onchange('planned_duration', 'date_start')
-    # def _inverse_planned_duration(self):
-    #     for r in self:
-    #         working_days = len(pd.bdate_range(r.date_start.strftime('%Y-%m-%d'),
-    #                                               r.date_end.strftime('%Y-%m-%d')))
-    #         off_day = (r.date_end - r.date_start).days - working_days
-    #         if r.date_start and r.planned_duration and not r.env.context.get('ignore_onchange_planned_duration', False):
-    #             r.date_end = r.date_start + timedelta(days=r.planned_duration+off_day)
+    @api.onchange('planned_duration', 'date_start')
+    def _inverse_planned_duration(self):
+        for r in self:
+            if r.date_start and r.planned_duration and not r.env.context.get('ignore_onchange_planned_duration', False):
+                r.date_end = r.date_start + timedelta(days=r.planned_duration)
 
     @api.depends('dependency_task_ids')
     def _compute_recursive_dependency_task_ids(self):
