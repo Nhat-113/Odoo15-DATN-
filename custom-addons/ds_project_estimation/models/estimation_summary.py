a9902d19f40f6176197c7f3947dc614e3401bdf7
dientd <dientd@d-soft.com.vn>
2022-06-03 11:38:35 +0700

    Auto compute cost rate by currency

--- custom-addons/ds_project_estimation/models/estimation_summary.py
+++ custom-addons/ds_project_estimation/models/estimation_summary.py
@@ -13,26 +13,27 @@ class EstimationSummaryTotalCost(models.Model):
     sequence = fields.Integer(string="No", index=True, readonly=True, help='Use to arrange calculation sequence',
                               default=1)
     module = fields.Char(string="Components", )
-    design_effort = fields.Float(string="Design", compute='_compute_effort')
-    dev_effort = fields.Float(string="Developer", compute='_compute_effort')
-    tester_effort = fields.Float(string="Tester", compute='_compute_effort')
-    comtor_effort = fields.Float(string="Comtor", compute='_compute_effort')
-    brse_effort = fields.Float(string="Brse", compute='_compute_effort')
-    pm_effort = fields.Float(string="PM", compute='_compute_effort')
+    design_effort = fields.Float(string="Design", compute='_compute_effort', store=True)
+    dev_effort = fields.Float(string="Developer", compute='_compute_effort', store=True)
+    tester_effort = fields.Float(string="Tester", compute='_compute_effort', store=True)
+    comtor_effort = fields.Float(string="Comtor", compute='_compute_effort', store=True)
+    brse_effort = fields.Float(string="Brse", compute='_compute_effort', store=True)
+    pm_effort = fields.Float(string="PM", compute='_compute_effort', store=True)
 
-    total_effort = fields.Float(string="Total Effort (MD)", readonly=True, compute='_compute_total_effort')
-    cost = fields.Float(string="Cost", readonly=True, compute='_compute_total_effort')
+    total_effort = fields.Float(string="Total Effort (MD)", readonly=True, compute='_compute_total_effort', store=True)
+    cost = fields.Float(string="Cost", readonly=True, compute='_compute_total_effort', store=True)
 
     @api.depends('estimation_id')
     def _compute_effort(self):
-        estimation_id = self.connect_summary.id
-        activities = self.env['module.effort.activity'].search([('estimation_id', '=', estimation_id)])
-        design_total = 0.0
-        dev_total = 0.0
-        tester_total = 0.0
-        comtor_total = 0.0
-        pm_total = 0.0
-        brse_total = 0.0
+        for item in self:
+            estimation_id = item.connect_summary.id
+            activities = item.env['module.effort.activity'].search([('estimation_id', '=', estimation_id)])
+            design_total = 0.0
+            dev_total = 0.0
+            tester_total = 0.0
+            comtor_total = 0.0
+            pm_total = 0.0
+            brse_total = 0.0
 
         for activity in activities:
             breakdown_activity = self.env['module.breakdown.activity'].search(
@@ -61,29 +62,30 @@ class EstimationSummaryTotalCost(models.Model):
         self.brse_effort = brse_total
 
     def _compute_total_effort(self):
-        self.total_effort = self.design_effort + self.dev_effort + self.tester_effort + self.comtor_effort + \
-                            self.pm_effort + self.brse_effort
-        connect_summary = self.connect_summary.ids
+        for item in self:
+            item.total_effort = item.design_effort + item.dev_effort + item.tester_effort + item.comtor_effort + \
+                                item.pm_effort + item.brse_effort
+            connect_summary = item.connect_summary.ids
 
-        cost_designer = self.design_effort * self.env['estimation.summary.costrate'].search(
-            [('types', '=', 'Designer'), ('connect_summary_costrate', 'in', connect_summary)]).yen_month
+            cost_designer = item.design_effort * self.env['estimation.summary.costrate'].search(
+                [('types', '=', 'Designer'), ('connect_summary_costrate', 'in', connect_summary)]).yen_month
 
-        cost_dev = self.dev_effort * self.env['estimation.summary.costrate'].search(
-            [('types', '=', 'Developer'), ('connect_summary_costrate', 'in', connect_summary)]).yen_month
+            cost_dev = item.dev_effort * self.env['estimation.summary.costrate'].search(
+                [('types', '=', 'Developer'), ('connect_summary_costrate', 'in', connect_summary)]).yen_month
 
-        cost_tester = self.tester_effort * self.env['estimation.summary.costrate'].search(
-            [('types', '=', 'Tester'), ('connect_summary_costrate', 'in', connect_summary)]).yen_month
+            cost_tester = item.tester_effort * self.env['estimation.summary.costrate'].search(
+                [('types', '=', 'Tester'), ('connect_summary_costrate', 'in', connect_summary)]).yen_month
 
-        cost_comtor = self.comtor_effort * self.env['estimation.summary.costrate'].search(
-            [('types', '=', 'Comtor'), ('connect_summary_costrate', 'in', connect_summary)]).yen_month
+            cost_comtor = item.comtor_effort * self.env['estimation.summary.costrate'].search(
+                [('types', '=', 'Comtor'), ('connect_summary_costrate', 'in', connect_summary)]).yen_month
 
-        cost_brse = self.brse_effort * self.env['estimation.summary.costrate'].search(
-            [('types', '=', 'Brse'), ('connect_summary_costrate', 'in', connect_summary)]).yen_month
+            cost_brse = item.brse_effort * self.env['estimation.summary.costrate'].search(
+                [('types', '=', 'Brse'), ('connect_summary_costrate', 'in', connect_summary)]).yen_month
 
-        cost_pm = self.pm_effort * self.env['estimation.summary.costrate'].search(
-            [('types', '=', 'Project manager'), ('connect_summary_costrate', 'in', connect_summary)]).yen_month
+            cost_pm = item.pm_effort * self.env['estimation.summary.costrate'].search(
+                [('types', '=', 'Project manager'), ('connect_summary_costrate', 'in', connect_summary)]).yen_month
 
-        self.cost = cost_designer + cost_dev + cost_tester + cost_comtor + cost_brse + cost_pm
+            item.cost = cost_designer + cost_dev + cost_tester + cost_comtor + cost_brse + cost_pm
 
 
 class EstimationSummaryCostRate(models.Model):
@@ -96,39 +98,35 @@ class EstimationSummaryCostRate(models.Model):
     sequence = fields.Integer(string="No", )
     types = fields.Char(string="Type", )
     role = fields.Many2one('cost.rate', string='Role')
-    currency_id = fields.Many2one('estimation.currency', store=True, )  # compute='_compute_currency_id'
+    currency_ids = fields.Many2one('estimation.work', store=True, )  # compute='_compute_currency_id'
     yen_month = fields.Float(string="Unit (Currency/Month)", store=True, compute='_compute_yen_month', default=0.00)
     yen_day = fields.Float(string="Unit (Currency/Day)", store=True, compute='_compute_yen_month', default=0.00)
-    vnd_day = fields.Float(string="Unit (VND/Day)", store=True, compute='_compute_yen_month', default=0.00)
 
-    # def _compute_currency_id(self):
-    #     for item in self:
-    #         estimation_id = self.connect_summary_costrate.id
-    #         if type(estimation_id) != 'int':
-    #             estimation_id = self.connect_summary_costrate.ids[0]
-    #         item.currency_id = self.env['estimation.work'].search([('id', '=', estimation_id)]).currency_id
-
-    @api.depends('role')
+    @api.depends('role', 'connect_summary_costrate.currency_id',)
     def _compute_yen_month(self):
         for item in self:
             cost_rate = self.env['cost.rate'].search([('id', '=', item.role.id)])
-            if cost_rate:
-                estimation_id = self.connect_summary_costrate.id
-                if type(estimation_id) != 'int':
-                    estimation_id = self.connect_summary_costrate.ids[0]
-                currency_id = self.env['estimation.work'].search([('id', '=', estimation_id)]).currency_id
-                value_exchange = self.env['estimation.exchange.rate'].search(
-                    [('currency_id', '=', currency_id.id)]).value
-                if currency_id.id == 1:
-                    item.yen_month = cost_rate.cost_usd
-                    item.yen_day = cost_rate.cost_usd / 20
-                    item.vnd_day = cost_rate.cost_vnd / 20
-                else:
-                    item.yen_month = cost_rate.cost_usd * value_exchange
-                    item.yen_day = cost_rate.cost_usd * value_exchange / 20
-                    item.vnd_day = cost_rate.cost_vnd / 20
-            else:
-                item.yen_month = 0.0
-                item.yen_day = 0.0
-                item.vnd_day = 0.0
 
+            if item.connect_summary_costrate.id:
+                currency_id = item.connect_summary_costrate.currency_id.id
+
+            elif item.connect_summary_costrate.id == False:
+                currency_id = item.connect_summary_costrate.currency_id.id
+
+            elif item.connect_summary_costrate.id.origin:
+                currency_id = item.connect_summary_costrate.currency_id.id
+
+            else:
+                currency_id = item.connect_summary_costrate.currency_id.id
+                if currency_id == False:
+                    currency_id = 1
+
+            if currency_id == 1:
+                item.yen_month = cost_rate.cost_usd
+                item.yen_day = cost_rate.cost_usd / 20
+            elif currency_id == 22:
+                item.yen_month = cost_rate.cost_vnd
+                item.yen_day = cost_rate.cost_vnd / 20
+            else:
+                item.yen_month = cost_rate.cost_yen
+                item.yen_day = cost_rate.cost_yen / 20
