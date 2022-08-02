65ec26ef542ba56a71fb4cb50d029b4f9a8d6f0d
dientd <dientd@d-soft.com.vn>
2022-08-02 14:56:00 +0700

    Update API attendances and New addon Auth api key

--- custom-addons/hr_attendances/controllers/main.py
+++ custom-addons/hr_attendances/controllers/main.py
@@ -4,12 +4,22 @@
 from urllib import response
 from odoo import http
 from odoo.http import request
-
+from datetime import datetime, timedelta 
+import datetime
+import json
 
 class HrAttendance(http.Controller):
-    @http.route('/facelog_attendances', auth='user', type='json')
+    @http.route('/api/facelog_attendances',auth='api_key', type='json')
     def facelog_attendances(self, **kw):
-        print('--check api---')
-        employee = request.env['hr.employee'].search([('id','=',kw['employee_id'])])
-        request.env['hr.employee'].attendance_manual_api(employee, kw['next_action'])
-        return {'200 Suscess'}
+
+        employees = request.env['hr.employee'].search([('work_email','like',kw['nick_name']),('name','=',kw['first_name']+' '+kw['middle_name']+' '+kw['last_name']),('mobile_phone','like', kw['phone'])])
+        if len(employees)==1:
+            # convert string to datetime
+            date_time = datetime.datetime.strptime(kw['date_time'], "%m/%d/%Y, %H:%M:%S")
+            # convert local+7 to UTC+0
+            date_time = date_time - timedelta(hours = 7)
+            result = request.env['hr.employee'].attendance_manual_api(employees, date_time, "hr_attendance.hr_attendance_action_my_attendances")
+            response_message = result['action']['attendance']
+            return response_message
+        else:
+            return  "User does not exist"
--- custom-addons/hr_attendances/models/hr_attendances.py
+++ custom-addons/hr_attendances/models/hr_attendances.py
@@ -5,16 +5,15 @@ from odoo.tools import float_round
 class HrEmployee(models.Model):
     _inherit = "hr.employee"
 
-    def attendance_manual_api(self, employee, next_action):
-        print(employee)
+    def attendance_manual_api(self, employee, date_time, next_action):
         employee.ensure_one()
         can_check_without_pin = not employee.env.user.has_group('hr_attendance.group_hr_attendance_use_pin') or (employee.user_id == employee.env.user and entered_pin is None)
         if can_check_without_pin or entered_pin is not None and entered_pin == employee.sudo().pin:
-            return employee._attendance_action(next_action)
+            return employee._attendance_action_api(next_action, date_time)
         return {'warning': _('Wrong PIN')}
 
 
-    def _attendance_action(self, next_action):
+    def _attendance_action_api(self, next_action, date_time):
         """ Changes the attendance of the employee.
             Returns an action to the check in/out message,
             next_action defines which menu the check in/out message should return to. ("My Attendances" or "Kiosk Mode")
@@ -29,10 +28,32 @@ class HrEmployee(models.Model):
         action_message['hours_today'] = employee.hours_today
 
         if employee.user_id:
-            modified_attendance = employee.with_user(employee.user_id)._attendance_action_change()
+            modified_attendance = employee.with_user(employee.user_id)._attendance_action_change_api(date_time)
         else:
-            modified_attendance = employee._attendance_action_change()
+            modified_attendance = employee._attendance_action_change_api(date_time)
         action_message['attendance'] = modified_attendance.read()[0]
         action_message['total_overtime'] = employee.total_overtime
 
         return {'action': action_message}
+
+    def _attendance_action_change_api(self, date_time):
+        """ Check In/Check Out action
+            Check In: create a new attendance record
+            Check Out: modify check_out field of appropriate attendance record
+        """
+        self.ensure_one()
+        action_date = date_time
+
+        if self.attendance_state != 'checked_in':
+            vals = {
+                'employee_id': self.id,
+                'check_in': action_date,
+            }
+            return self.env['hr.attendance'].create(vals)
+        attendance = self.env['hr.attendance'].search([('employee_id', '=', self.id), ('check_out', '=', False)], limit=1)
+        if attendance:
+            attendance.check_out = action_date
+        else:
+            raise exceptions.UserError(_('Cannot perform check out on %(empl_name)s, could not find corresponding check in. '
+                'Your attendances have probably been modified manually by human resources.') % {'empl_name': self.sudo().name, })
+        return attendance
