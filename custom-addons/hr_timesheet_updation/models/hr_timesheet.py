f23397d2049046a12d920b7a6c576fb5ac10d5e1
lethuan-04102000 <thedanh0410@gmail.com>
2022-08-18 11:28:04 +0700

    [hr_time_sheet][762] fix bug Not display all projects that assigned for employee

--- custom-addons/hr_timesheet_updation/models/hr_timesheet.py
+++ custom-addons/hr_timesheet_updation/models/hr_timesheet.py
@@ -5,7 +5,15 @@ from odoo import api, Command, fields, models, _
 
 class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
+    
+    def _domain_project_id(self):
+        domain = [('allow_timesheets', '=', True)]
+        return domain
 
+    project_id = fields.Many2one(
+        'project.project', 'Project', compute='_compute_project_id', store=True, readonly=False,
+        domain=_domain_project_id)    
+            
     @api.model
     def default_get(self, field_list):
         result = super(AccountAnalyticLine, self).default_get(field_list)
@@ -15,6 +23,4 @@ class AccountAnalyticLine(models.Model):
             result['employee_id'] = self.env['hr.employee'].search([('user_id', '=', result['user_id'])], limit=1).id
        
         return result
-
-
         
