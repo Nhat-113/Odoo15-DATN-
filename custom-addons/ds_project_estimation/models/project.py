531601fea9be578d265c4b3db716bd2ecc9867b0
dientd <dientd@d-soft.com.vn>
2022-08-18 15:50:52 +0700

    [Project][Edit] Thêm Total Cost cho Estimation và có thể chọn nhiều Estimation cho một project

--- custom-addons/ds_project_estimation/models/project.py
+++ custom-addons/ds_project_estimation/models/project.py
@@ -1,4 +1,5 @@
-from odoo import models, fields, api
+from odoo import models, fields
+import json
 
 class Project(models.Model):
     """
@@ -6,4 +7,19 @@ class Project(models.Model):
     """
     _inherit = 'project.project'
     
-    estimation_id = fields.Many2one('estimation.work', string="Estimation")
+    def _compute_estimation_id_domain(self):
+        list_estimation = []
+        project = self.env['project.project'].search([])
+        for record in project:
+            list_estimation.append(record.estimation_id.ids)
+
+        flat_list = [item for sublist in list_estimation for item in sublist]
+        
+        self.estimation_id_domain = json.dumps(
+                [('id', 'not in', flat_list)]
+            )
+
+    estimation_id_domain = fields.Char(compute="_compute_estimation_id_domain", readonly=True, store=False,)
+    estimation_id = fields.Many2many('estimation.work', string="Estimation", domain="estimation_id_domain")
+    
+    
