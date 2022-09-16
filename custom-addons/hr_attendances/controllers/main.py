b8c5ab3571d0854fc5e7faf6c349a1c5bddd5e6a
dientd <dientd@d-soft.com.vn>
2022-09-16 16:41:26 +0700

    [Hr Attendances]: Update format respone api, fixbug [1125,1113,1110,1105] and add api get public-holiday from time-off

--- custom-addons/hr_attendances/controllers/main.py
+++ custom-addons/hr_attendances/controllers/main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from odoo import http
+from odoo.addons.test_impex.tests.test_load import message
 from odoo.http import request
 from datetime import datetime, timedelta 
 from odoo.addons.web.controllers.main import Session
@@ -11,36 +12,39 @@ import datetime
 class HrAttendance(http.Controller):
     @http.route('/api/facelog_attendances',auth='api_key', type='json')
     def facelog_attendances(self, **kw):
-        # Fixme: get config domain email with comapy
-        email = kw['nick_name'] + '@d-soft.com.vn'
-        employees = request.env['hr.employee'].search([('work_email','=',email)])
-        if len(employees):
-            # convert string to datetime
-            try:
-                date_time = datetime.datetime.strptime(kw['date_time'], "%m/%d/%Y, %H:%M:%S")
-            except:
-                return {"status":500, "Message": "Value input does not match format '%m/%d/%Y, %H:%M:%S"}
-            # convert local+7 to UTC+0
-            date_time = date_time - timedelta(hours = 7)
-            if kw['is_checkin']=='True' or kw['is_checkin']=='False':
-                result = request.env['hr.employee'].attendance_manual_api(employees, date_time, "hr_attendance.hr_attendance_action_my_attendances", kw['is_checkin'])
-            else: 
-                return {"status":404, "message":"missing is_checkin param or value does not match fomat bool"}
-            
-            response_message = result['action']['attendance']
-
-            response = {
-                "id": response_message['id'],
-                "employee_id": response_message['employee_id'],
-                "check_in": response_message['check_in'],
-                "check_out": response_message['check_out'],
-                "worked_hours": response_message['worked_hours']
-            }
-
-
-            return {"status": 200, "message": response}
+        if kw['is_checkin']=="" or kw['date_time']=="" or kw['nick_name']=="":
+            return {
+                "status": 400, 
+                "message": "Missing parameter"
+                }
         else:
-            return  {"status": 400, "message": "User does not exist"}
+            # Fixme: get config domain email with comapy
+            email = kw['nick_name'] + '@d-soft.com.vn'
+            employees = request.env['hr.employee'].search([('work_email','=',email)])
+            if len(employees):
+                # convert string to datetime
+                try:
+                    date_time = datetime.datetime.strptime(kw['date_time'], "%m/%d/%Y, %H:%M:%S")
+                except:
+                    return {"status": 500, "Message": "Value input does not match format '%m/%d/%Y, %H:%M:%S"}
+                # convert local+7 to UTC+0
+                date_time = date_time - timedelta(hours = 7)
+                if kw['is_checkin']=='True' or kw['is_checkin']=='False':
+                    result = request.env['hr.employee'].attendance_manual_api(employees, date_time, "hr_attendance.hr_attendance_action_my_attendances", kw['is_checkin'])
+                else: 
+                    return {"status":404, "message": "is_checkin param does not match format boolean"}
+                
+                response_message = result['action']['attendance']
+                response = {
+                    "id": response_message['id'],
+                    "employee_id": response_message['employee_id'],
+                    "check_in": response_message['check_in'],
+                    "check_out": response_message['check_out'],
+                    "worked_hours": response_message['worked_hours']
+                }
+                return {"status": 200, "message": response}
+            else:
+                return  {"status": 400, "message": "User does not exist"}
 
 
     @http.route('/api/attendances/getuser', auth='api_key', type='json')
@@ -61,21 +65,63 @@ class HrAttendance(http.Controller):
                 }
             return response
         except:
-            if not kw["nick_name"]:
+            if not kw["nick_name"] and not kw:
                 return {"status": 404, "message": "Missing nick_name parameter"}
             else:
-                return {"status": 400, "massage": "Bad requests"}
+                return {"status": 400, "massage": "Missing parameter"}
+
+    @http.route('/api/attendances/get_public_holiday', auth='public', type='json')
+    def facelog_get_public_holiday(self, **kw):
+        try:
+            if kw:
+                return {
+                    "status": 400,
+                    "massage": "No need any parameters",
+                }
+            else:
+                vals = []
+                public_holiday = request.env['resource.calendar.leaves'].search([('calendar_id','=',False),('holiday_id','=',False)])
+                for record in public_holiday:
+                    holiday = {
+                                'item': record.name,
+                                'date_from': record.date_from,
+                                'date_to': record.date_to
+                                }
+                    vals.append(holiday)
+
+                return {
+                    "status": 200,
+                    "message": "Request success",
+                    "values": vals
+                }
+        except:
+            return {
+                    "status": 500,
+                    "massage": "Odoo Server Error",
+                }
+
 
 class SessionCustom(Session):
     @http.route('/web/session/authenticate/api_attendances_key', type='json', auth='public', sitemap=False)
-    def authenticate(self, uid):
+    def authenticate(self, **uid):
         try:
-            api_key = request.env['auth.api.key'].search([('user_id','=',uid)], limit=1).key
+            if uid:
+                user = request.env['auth.api.key'].search([('user_id','=',uid)], limit=1)
+                api_key = user.key
+            else:
+                return {"status": 404, "message": "Missing uid parameter"}
         except:
             api_key = None
         
-        try:
-            value = {"status": 200, "value": {"message": "Connect Success", "api_key": api_key}}
+        if api_key:
+            value = {
+                "status": 200, 
+                "value": {
+                    "message": "Connect Success", 
+                    "api_key": api_key,
+                    "user": user.user_id.display_name
+                    }
+                }
             return value
-        except:
-            return {"status": 400, "message":"Bad request"}
+        else: 
+            return {"status": 400, "message": "Nothing api key mapping with uid parameter"}
