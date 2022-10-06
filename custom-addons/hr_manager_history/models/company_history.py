57a03c44bdffb29d509240444bbaa65697c7a6b0
dientd <dientd@d-soft.com.vn>
2022-10-06 23:04:57 +0700

    [Hr Manager History]: Update view tree selete button and auto gen email with representative

--- custom-addons/hr_manager_history/models/company_history.py
+++ custom-addons/hr_manager_history/models/company_history.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import api, models, fields
 
 class CompanyHistory(models.Model):
     _name = 'hr.company.history'
@@ -6,5 +6,10 @@ class CompanyHistory(models.Model):
     company_id = fields.Many2one('res.company', string="Company", readonly=True)
     date_start = fields.Date('Date Start', readonly=False)
     date_end = fields.Date('Date End', readonly=False)
-    email_history_id = fields.Char(string='Email Representative History')
+    email_history_id = fields.Char(string='Email Representative History', compute="_compute_email_with_representative", store=True)
     representative = fields.Many2one('hr.employee', "Representative")
+
+    @api.depends('representative')
+    def _compute_email_with_representative(self):
+        for record in self:
+            record.email_history_id = self.env['hr.employee'].search([('id','=',record.representative.id)]).work_email
