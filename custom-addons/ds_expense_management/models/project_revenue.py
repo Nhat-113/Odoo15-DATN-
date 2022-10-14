289090ed33c9e1ee19601f0098b8ec9009c68adc
unknown <vietlx@d-soft.com.vn>
2022-10-14 17:43:06 +0700

    [Company Expense] Adding SGD currency for Project cost, Project revenue & fix Project cost from Cost management

--- custom-addons/ds_expense_management/models/project_revenue.py
+++ custom-addons/ds_expense_management/models/project_revenue.py
@@ -22,14 +22,17 @@ class ProjectRevenue(models.Model):
     
     rounding_usd_input = fields.Float(string="USD to VND", tracking=True)
     rounding_jpy_input = fields.Float(string="JPY to VND", tracking=True)
+    rounding_sgd_input = fields.Float(string="SGD to VND", tracking=True)
     
     currency_id = fields.Many2one('res.currency', string="Currency", required=True, default=lambda self: self.env.ref('base.main_company').currency_id, tracking=True)
     currency_usd = fields.Many2one('res.currency', string="USD Currency", default=lambda self: self._get_default_currency('USD'), readonly=True)    
     currency_jpy = fields.Many2one('res.currency', string="YPY Currency", default=lambda self: self._get_default_currency('JPY'), readonly=True)   
+    currency_sgd = fields.Many2one('res.currency', string="SGD Currency", default=lambda self: self._get_default_currency('SGD'), readonly=True)
     currency_vnd = fields.Many2one('res.currency', string="VND Currency", default=lambda self: self._get_default_currency('VND'), readonly=True)   
     
     revenue_usd = fields.Monetary(string="Total Revenue", currency_field='currency_usd', store=True, readonly=True, compute="_convert_currency_revenue")
     revenue_jpy = fields.Monetary(string="Total Revenue", currency_field='currency_jpy', store=True, readonly=True)
+    revenue_sgd = fields.Monetary(string="Total Revenue", currency_field='currency_sgd', store=True, readonly=True)
     revenue_vnd = fields.Monetary(string="Total Revenue", currency_field='currency_vnd', store=True, readonly=True)
     
     get_currency_name = fields.Char(string='Currency Name', readonly=True, store=True)
@@ -40,7 +43,7 @@ class ProjectRevenue(models.Model):
             record.start_date = record.project_id.date_start
             record.end_date = record.project_id.date
 
-    @api.depends('revenue_project', 'rounding_usd_input', 'rounding_jpy_input', 'currency_id')
+    @api.depends('revenue_project', 'rounding_usd_input', 'rounding_jpy_input', 'rounding_sgd_input', 'currency_id')
     def _convert_currency_revenue(self):
         for record in self:
             record.get_currency_name = record.currency_id.name
@@ -55,6 +58,10 @@ class ProjectRevenue(models.Model):
                         record.revenue_jpy = record.revenue_project / record.rounding_jpy_input
                     else:
                         record.revenue_jpy = 0
+                    if record.rounding_sgd_input != 0.0:
+                        record.revenue_sgd = record.revenue_project / record.rounding_sgd_input
+                    else:
+                        record.revenue_sgd = 0 
                         
                 elif record.currency_id.name == 'JPY':   
                     record.revenue_jpy = record.revenue_project
@@ -63,6 +70,10 @@ class ProjectRevenue(models.Model):
                     #     record.revenue_usd = record.revenue_vnd / record.rounding_usd_input
                     # else:
                     #     record.revenue_usd = 0
+                
+                elif record.currency_id.name == 'SGD':
+                    record.revenue_sgd = record.revenue_project
+                    record.revenue_vnd = record.revenue_project * record.rounding_sgd_input
                     
                 else:
                     record.revenue_usd = record.revenue_project
@@ -71,6 +82,7 @@ class ProjectRevenue(models.Model):
             else:
                 record.revenue_usd = 0
                 record.revenue_jpy = 0
+                record.revenue_sgd = 0
                 record.revenue_vnd = 0
         
     @api.onchange('company_id')
