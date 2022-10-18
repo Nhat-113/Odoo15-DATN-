c0dded32cda8e3553d5bbe631baff7744ab46748
dientd <dientd@d-soft.com.vn>
2022-10-18 11:56:46 +0700

    [Request Overtime]: Update function for flow request overtime

--- custom-addons/hr_timesheet_request_overtime/models/hr_request_overtime.py
+++ custom-addons/hr_timesheet_request_overtime/models/hr_request_overtime.py
@@ -19,20 +19,26 @@ class HrRequestOverTime(models.Model):
     _description = "Hr Request Overtime"
     _order = "id DESC"
 
+    @api.model
+    def _get_default_stage_id(self):
+        return self.env['hr.request.overtime.stage'].search([], limit=1).id
+
     project_id = fields.Many2one('project.project', string="Project", required=True, domain="[('company_id', '=', company_id)]", tracking=True)
-    start_date = fields.Date(string="Start date", required=False, tracking=True, store=True)
-    end_date = fields.Date(string="End date", required=False, tracking=True, store=True)
+    start_date = fields.Date(string="Start Date", required=False, tracking=True, store=True)
+    end_date = fields.Date(string="End Date", required=False, tracking=True, store=True)
     company_id = fields.Many2one('res.company', string="Company", required=True, default=lambda self: self.env.company, tracking=True)
     description = fields.Text(string="Description", tracking=True)
-    stage_id = fields.Many2one('hr.request.overtime.stage', 'Stage', ondelete='restrict', tracking=True,
-                            # compute='_compute_stage', 
+    stage_id = fields.Many2one('hr.request.overtime.stage', 'Stage', ondelete='restrict',
+                            default=_get_default_stage_id, 
                             store=True, readonly=False,
+                            group_expand='_read_group_stage_ids',
+                            required=True, tracking=True,
                             # domain=lambda self: self._compute_domain_stage(),
                             copy=False, index=True,
-                            # group_expand='_read_group_stage_ids'
                             )
+    requester_id = fields.Many2one('res.users', string='Requester', readonly=False)
 
-    user_id = fields.Many2one('res.users', string='Project Manager', default=lambda self: self.env.user, tracking=True)
+    user_id = fields.Many2one('res.users', string='Project Manager', tracking=True, readonly=True, compute='_compute_project_manager')
     active = fields.Boolean(string='Invisible Refuse Button', default=True)
     member_ids = fields.Many2many('res.users', string='Members',
                                   help="All members has been assigned to the project", tracking=True)
@@ -42,3 +48,20 @@ class HrRequestOverTime(models.Model):
 
     def archive_request_overtime(self):
         print('-------------------------------------')
+
+    @api.model
+    def _read_group_stage_ids(self, stages, domain, order):
+        """ Always display all stages """
+        return stages.search([], order=order)
+
+    @api.onchange('booking_overtime')
+    def _add_booking_overtime(self):
+        # update member_ids list
+        user_ids = [
+            user.user_id.id for user in self.booking_overtime.employee_id]
+        self.member_ids = self.env['res.users'].search([('id', 'in', user_ids)])
+
+    @api.onchange('project_id')
+    def _compute_project_manager(self):
+        for item in self:
+            item.user_id = item.project_id.user_id or False
