a6270479f438269aa7ebc97353327de545765e2b
unknown <vietlx@d-soft.com.vn>
2022-11-14 17:01:18 +0700

    [project updation] update feature validate duration project for mapping company expense

--- custom-addons/project_updation/models/project.py
+++ custom-addons/project_updation/models/project.py
@@ -403,7 +403,7 @@ class Project(models.Model):
             for project_revenue in project_revenues:
                 list_dates.append(date(int(project_revenue.get_year), int(project_revenue.get_month), 1))
             
-            project_expenses = self.env['project.expense.value'].sudo().search([('project_id', '=', project.id)])
+            project_expenses = self.env['project.expense.value'].sudo().search([('project_id', '=', project.id), ('project_expense_management_id', '!=', False)])
             for project_expense in project_expenses:
                 list_dates.append(project_expense.expense_date)
 
