a9cf87b160deecc4c080c3a537a67db747d09a35
dientd <dientd@d-soft.com.vn>
2022-11-18 14:18:52 +0700

    [Request Overtime]: Remove UI view

--- odoo/service/server.py
+++ odoo/service/server.py
@@ -440,7 +440,7 @@ class ThreadedServer(CommonServer):
 
                 registries = odoo.modules.registry.Registry.registries
                 _logger.debug('cron%d polling for jobs', number)
-                for db_name, registry in registries.d.items():
+Trainee                for db_name, registry in registries.d.items():
                     if registry.ready:
                         thread = threading.currentThread()
                         thread.start_time = time.time()
