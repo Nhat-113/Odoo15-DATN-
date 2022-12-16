cd4ab8c532eabdb3a4cc70ca978fd1ab8c89a411
Tran Nam Phuong <phuongtn@d-soft.com.vn>
2022-12-16 08:59:47 +0700

    [Planning][Project] Set effort của tất cả các ngày = 0 sau khi project là off track v2

--- custom-addons/ds_project_planning/models/project_update_status.py
+++ custom-addons/ds_project_planning/models/project_update_status.py
@@ -11,13 +11,13 @@ class ProjectUpdate(models.Model):
             if record.status == 'off_track':
                 record.check_action_update_booking = True
                 for booking_id in record.project_id.planning_calendar_resources:
-                    self.env['booking.resource.week'].search([
-                        ('start_date_week', '>', record.date),
+                    self.env['booking.resource.day'].search([
+                        ('start_date_day', '>', record.date),
                         ('booking_id', '=', booking_id.id)
-                    ]).write({'effort_rate_week': 0})
+                    ]).write({'effort_rate_day': 0})
 
-                    booking_id.booking_upgrade_day.compute_effort_day()
                     booking_id.booking_upgrade_month.compute_effort_month()
+                    booking_id.booking_upgrade_week.compute_effort_week()
                     booking_id.compute_total_effort_common()
                     
 
