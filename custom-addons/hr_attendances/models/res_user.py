744056bb3f975baff3011d7f1371a10249a585a0
xuandiem2709 <xdiem2709@gmail.com>
2024-05-27 14:07:23 +0700

    [hr_attendances] optimize code

--- custom-addons/hr_attendances/models/res_user.py
+++ custom-addons/hr_attendances/models/res_user.py
@@ -6,9 +6,8 @@ class ResUsers(models.Model):
     
     @api.constrains('active')
     def _remove_api_token(self):
-        if self[0].active  == False:
+        if self[0].active == False:
             auth_api_token = self.env['auth.api.token'].search([('user_id', 'in', self.ids)])
             auth_api_key = self.env['auth.api.key'].search([('user_id', 'in', self.ids)])
-            if auth_api_token or auth_api_key:
-                auth_api_token.unlink()
-                auth_api_key.unlink()
+            auth_api_token.unlink()
+            auth_api_key.unlink()
