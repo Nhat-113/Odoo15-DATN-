842252ea4d851d3176a9b55c4d2ca23c4b802451
Nhat-113 <nhatvo1302@gmail.com>
2024-06-07 09:33:00 +0700

    [tenant_mobile_management] add chatter

--- custom-addons/tenant_mobile_management/models/tenant.py
+++ custom-addons/tenant_mobile_management/models/tenant.py
@@ -1,15 +1,36 @@
 # odoo/models/tenant.py
-from odoo import models, fields
+from odoo import models, fields, api
 
 class Tenant(models.Model):
     _name = 'tenant.management'
     _description = "Tenat Mobile Management"
     _rec_name = "server_name"
-    _order = "id"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
+    _order = "sequence, id"
 
-    server_name = fields.Char(string='Server Name', required=True)
-    link_domain = fields.Char(string='Link Domain', required=True)
-    description = fields.Text(string='Description')
+    server_name = fields.Char(string= 'Server Name', required= True, tracking= True)
+    link_domain = fields.Char(string= 'Link Domain', required= True, tracking= True)
+    description = fields.Text(string= 'Description', tracking= True)
+    sequence = fields.Integer(string= 'No.')
+
+    @api.model
+    def create(self, vals):
+        if not vals.get('sequence'):
+            last_sequence = self.search([], order='sequence desc', limit=1)
+            vals['sequence'] = last_sequence.sequence + 1 if last_sequence else 1
+        return super(Tenant, self).create(vals)
+
+    def unlink(self):
+        all_records = self.env['tenant.management'].search([], order='sequence')
+        result = super(Tenant, self).unlink()
+        all_records._update_sequences()
+        return result
+
+    @api.model
+    def _update_sequences(self):
+        tenants = self.search([], order='sequence')
+        for index, tenant in enumerate(tenants):
+            tenant.write({'sequence': index + 1})
 
     _sql_constraints = [
         ('server_name', 'unique(server_name)', 'Server name must be unique'),
