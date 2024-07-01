661890f8eea98b250a5cc2dd0c18d9250153c64a
ZaZuy <huynhduy25072002@gmail.com>
2024-07-01 15:52:25 +0700

    [project] Auto fill followers in project/task

--- custom-addons/ds_print_contract/models/hr_contract.py
+++ custom-addons/ds_print_contract/models/hr_contract.py
@@ -109,3 +109,12 @@ class HrContract(models.Model):
             'target': 'new',
             'context': ctx,
         }
+
+    # ------------------------------------------------------
+    # FOLLOWERS API
+    # ------------------------------------------------------
+    
+    # @Override
+    # Disable the feature of automatically adding the manager department to followers when changing the department from the hr.contract model
+    def _message_auto_subscribe(self, updated_values, followers_existing_policy='skip'):
+        return True
