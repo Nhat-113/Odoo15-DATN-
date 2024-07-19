dff49b0a4042888815b5bd9007096e927c3bf5b1
xuandiem2709 <xdiem2709@gmail.com>
2024-07-19 13:52:36 +0700

    [box_management] update layout and add status for setting schedule

--- helper/setting_device_common.py
+++ helper/setting_device_common.py
@@ -13,13 +13,13 @@ def get_day_of_week_value(record):
 def convert_string_to_time(time_string):
     return datetime.strptime(time_string, '%H:%M').time() 
     
-def create_update_setting(device_id, settings, start_time, end_time, week_day):
+def create_update_setting(device_id, settings, start_time, end_time, week_day, status, active):
     for rec in settings:
         start = convert_string_to_time(rec.start_time)
         end = convert_string_to_time(rec.end_time)
         rec_week_day = get_day_of_week_value(rec)
         
-        if is_overlapped_time([(start, end), (start_time, end_time)]):
+        if is_overlapped_time([(start, end), (start_time, end_time)]) and status == "active" and active:
             if any(x and y for x, y in zip(week_day, rec_week_day)):
                 raise exceptions.ValidationError(
                     _("Device ID %(id)s was set from %(start)s to %(end)s on the same days.", 
