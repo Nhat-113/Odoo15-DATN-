235cc93254c367c0990250ca8c0a052be84626f6
Nhat-113 <nhatvo1302@gmail.com>
2024-07-26 17:29:18 +0700

    [hr_attendances] fix and update data file export

--- custom-addons/time_off_support/models/res_company.py
+++ custom-addons/time_off_support/models/res_company.py
@@ -5,3 +5,4 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
     
     time_off_type_id = fields.Many2one('hr.leave.type', string='For Work From Home')
+    time_off_type_unpaid_id = fields.Many2many('hr.leave.type', string='For Unpaid')
--- custom-addons/time_off_support/models/res_config_settings.py
+++ custom-addons/time_off_support/models/res_config_settings.py
@@ -1,16 +1,19 @@
-    
 from odoo import models, fields, api
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
     
-    time_off_type_id = fields.Many2one('hr.leave.type', string='For Work From Home')
+    time_off_type_id = fields.Many2one('hr.leave.type')
+    time_off_type_unpaid_id = fields.Many2many('hr.leave.type')
 
     @api.model
     def get_values(self):
         res = super(ResConfigSettings, self).get_values()
         company = self.env.company
-        res.update({'time_off_type_id': company.time_off_type_id.id})
+        res.update({
+            'time_off_type_id': company.time_off_type_id.id,
+            'time_off_type_unpaid_id': [(6, 0, company.time_off_type_unpaid_id.ids)]
+        })
         return res
 
     def set_values(self):
@@ -18,3 +21,5 @@ class ResConfigSettings(models.TransientModel):
         company = self.env.company
         if self.time_off_type_id != company.time_off_type_id:
             company.write({'time_off_type_id': self.time_off_type_id.id})
+        if self.time_off_type_unpaid_id.ids != company.time_off_type_unpaid_id.ids:  
+            company.write({'time_off_type_unpaid_id': [(6, 0, self.time_off_type_unpaid_id.ids)]})
