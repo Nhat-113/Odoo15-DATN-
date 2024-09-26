471c628eafa2066b284bcaf4ba30c46cdf40602b
nhatck <nhatck@d-soft.com.vn>
2024-09-26 11:43:44 +0700

    [ds_company_management] fix group lead and div mng can view effort all departments in company

--- helper/company_management_common.py
+++ helper/company_management_common.py
@@ -1,28 +1,20 @@
-def get_children_departments(self, department_id):
-    children_departments = []
-    
-    direct_children = self.env['hr.department'].search([('parent_id', '=', department_id)]).ids
-    
-    for child in direct_children:
-        children_departments.append(child)
-        children_departments.extend(get_children_departments(self, child))
-    return children_departments
-    
+from helper.helper import get_children_departments
 
 def get_sql_by_department(self):
     current_user = self.env.user
     empl_id = current_user.employee_id.id
     sql_domain_parent_department = ' and department_id = null) '
     if empl_id:
-        is_department_manager = empl_id == current_user.department_id.manager_id.id
-        if is_department_manager:
-            departments = get_children_departments(self, current_user.department_id.id)
-            departments.append(current_user.department_id.id)
+        managing_departments = self.env['hr.department'].search([('manager_id', '=', empl_id)])
+        if managing_departments:
+            managing_departments_ids = managing_departments.ids
+            departments = get_children_departments(self, managing_departments_ids)
+            departments.extend(managing_departments_ids)
                 
             if len(departments) > 1:
-                sql_domain_parent_department = ' or department_id in ' + str(tuple(departments)) + ') '
+                sql_domain_parent_department = ' and department_id in ' + str(tuple(departments)) + ') '
             elif len(departments) == 1:
-                sql_domain_parent_department = ' or department_id = ' + str(departments[0]) + ') '
+                sql_domain_parent_department = ' and department_id = ' + str(departments[0]) + ') '
     return sql_domain_parent_department
 
 def is_ceo(current_user):
